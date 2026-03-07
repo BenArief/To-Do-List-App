ef4545019102b288b43cee2ecda17f6533621f33
BenArief <benarief29@gmail.com>
2026-03-07 16:43:40 +0700

    Generate quantity option

--- src/App.jsx
+++ src/App.jsx
@@ -38,16 +38,16 @@ function Header(){
 }
 
 function Form(){
+  const quantityNum = [...Array(10)].map((_, i) => (
+    <option value={i+1} key={i+1}>{i+1}</option>
+  ))
+
   return (
      <form className="add-form">
             <h3>Hari ini belanja apa kita?</h3>
             <div>
               <select>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
+               {quantityNum}
               </select>
               <input type="text" placeholder="nama barang..." />
             </div>
@@ -62,13 +62,7 @@ function GroceryList(){
           <div className="list">
           <ul>
             {groceryItems.map((item) => (
-              <li key={item.id}>
-                <input type="checkbox" checked="true" />
-                <span style={item.checked ? { textDecoration: 'line-through' } : {}}>
-                  {item.quantity} {item.name}
-                  </span>
-                <button>&times;</button>
-            </li> 
+             <Item item={item} key={item.id} />
             ))}
            
           </ul>
@@ -82,6 +76,18 @@ function GroceryList(){
           <button>Bersihkan Daftar</button>
         </div>
     </>
+  )
+}
+
+function Item({item}){
+  return (
+   <li key={item.id}>
+      <input type="checkbox" checked="true" />
+          <span style={item.checked ? { textDecoration: 'line-through' } : {}}>
+             {item.quantity} {item.name}
+          </span>
+      <button>&times;</button>
+            </li> 
   )
 }
 
